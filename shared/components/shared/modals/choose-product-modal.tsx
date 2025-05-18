dde0a6b960b33424fa09b63d4ca7384c7f99ee92
asecer <vrsxdcrown@gmail.com>
2025-05-18 15:10:44 +0300

    (modal) made a separate component with all the functionality for selecting a modal window (pizza/product)

--- shared/components/shared/modals/choose-product-modal.tsx
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -3,11 +3,8 @@
 import { cn } from '../../../lib'
 import { Dialog, DialogContent } from '../../ui'
 import { useRouter } from 'next/navigation'
-import { ChoosePizzaForm, ChooseProductForm } from '../'
+import { VariantsProduct } from '../'
 import { IProduct } from '@/@types/prisma'
-import { useCart, useModal } from '@/shared/store'
-import toast from 'react-hot-toast'
-import { sizes } from '@/shared/constants/pizza'
 
 type Props = {
 	className?: string
@@ -16,30 +13,6 @@ type Props = {
 
 export const ChooseProductModal = ({ className, product }: Props) => {
 	const router = useRouter()
-	const firstItem = product.variations[0]
-	const isPizzaForm = Boolean(firstItem.pizzaType) // если у продукта есть pizzaType значит это пицца, если нет, то что-то другое
-
-	const { addCartItem, status } = useCart(state => state)
-	const activeSize = useModal(state => state.activeSize)
-
-	const onSubmit = async (productItemId?: number, ingredients?: number[]) => {
-		try {
-			const itemId = productItemId ?? firstItem.id
-			await addCartItem({
-				productItemId: itemId,
-				ingredients,
-			})
-			toast.success(
-				`Добавлено: ${product.name} ${
-					firstItem.pizzaType ? `, ${sizes[activeSize].value} см` : ''
-				} `
-			)
-			router.back()
-		} catch (error) {
-			toast.error('Что-то пошло не так')
-			console.error(error)
-		}
-	}
 	return (
 		<>
 			<Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
@@ -49,29 +22,10 @@ export const ChooseProductModal = ({ className, product }: Props) => {
 						className
 					)}
 				>
-					{isPizzaForm ? (
-						<ChoosePizzaForm
-							imageUrl={product.imageUrl}
-							name={product.name}
-							ingredients={product.ingredients}
-							price={product.price as number}
-							desc={product.desc}
-							variations={product.variations}
-							onClickAdd={onSubmit}
-							status={status}
-						/>
-					) : (
-						<ChooseProductForm
-							imageUrl={product.imageUrl}
-							name={product.name}
-							ingredients={product.ingredients}
-							price={product.price as number}
-							desc={product.desc}
-							variations={product.variations}
-							onClickAdd={onSubmit}
-							status={status}
-						/>
-					)}
+					<VariantsProduct
+						product={product}
+						onCloseModal={() => router.back()}
+					/>
 				</DialogContent>
 			</Dialog>
 		</>
