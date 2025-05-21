76407d47b5abd58c22dc61526d1da91668159546
asecer <vrsxdcrown@gmail.com>
2025-05-21 21:44:44 +0300

    (design, home page) fixed the design of cards on the home page

--- shared/components/shared/product-card.tsx
+++ shared/components/shared/product-card.tsx
@@ -23,30 +23,34 @@ export const ProductCard = ({
 	return (
 		// <div className='max-w-[250px]'>
 		<Link href={`/product/${id}`}>
-			<div className='bg-[#fff7ee] py-6 px-[37px] rounded-2xl'>
-				<Image src={imageUrl} alt={name} width={292} height={292} />
-			</div>
-			<h4 className='pt-[15px] pb-2 text-[20px] font-bold '>{name}</h4>
-			<p className='text-[14px] text-[#b1b1b1] h-[100px]'>{desc}</p>
-			<div className='flex justify-between items-center'>
-				<p className='text-[20px]'>
-					{product.whProduct === 1 ? (
-						<>
-							<span>от</span>
+			<div className='flex flex-col justify-between h-full'>
+				<div className='bg-[#fff7ee] py-6 px-[37px] rounded-2xl'>
+					<Image src={imageUrl} alt={name} width={292} height={292} />
+				</div>
+				<h4 className='pt-[15px] pb-2 text-[20px] font-bold '>{name}</h4>
+				<p className='text-[14px] text-[#b1b1b1] pb-5 h-[100px]'>{desc}</p>
+				<div className='flex justify-between items-center'>
+					<p className='text-[20px]'>
+						{product.whProduct === 1 ? (
+							<>
+								<span>от</span>
+								<span className='text-[20px] font-bold'> {price} ₽</span>
+							</>
+						) : (
 							<span className='text-[20px] font-bold'> {price} ₽</span>
-						</>
-					) : (
-						<span className='text-[20px] font-bold'> {price} ₽</span>
-					)}
-				</p>
+						)}
+					</p>
 
-				{product.whProduct === 1 ? (
-					<Button variant={'secondary'} className='w-[120px] text-[16px]'>
-					Выбрать
-				</Button>
-				) : <Button variant={'secondary'} className='w-[120px] text-[16px]'>
-					В корзину
-				</Button>}
+					{product.whProduct === 1 ? (
+						<Button variant={'secondary'} className='w-[120px] text-[16px]'>
+							Выбрать
+						</Button>
+					) : (
+						<Button variant={'secondary'} className='w-[120px] text-[16px]'>
+							В корзину
+						</Button>
+					)}
+				</div>
 			</div>
 		</Link>
 	)
