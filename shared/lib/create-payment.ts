c2a18f738b58ba084b5f8096cc4ad0a897f0c3bd
asecer <vrsxdcrown@gmail.com>
2025-05-23 15:13:01 +0300

    (yookassa) connected yookassa and made a payment function (create-paymend.ts)

---
+++ shared/lib/create-payment.ts
@@ -0,0 +1,36 @@
+import { PaymentData } from '@/@types/yookassa'
+import axios from 'axios'
+
+export async function createPayment(details: any) {
+	const { data } = await axios.post<PaymentData>(
+		'https://api.yookassa.ru/v3/payments',
+		{
+			amount: {
+				value: details.amount,
+				currency: 'RUB',
+			},
+
+			capture: true,
+			description: details.description,
+			metadata: {
+				order_id: details.orderId,
+			},
+			confirmation: {
+				type: 'redirect',
+				return_url: process.env.YOOKASSA_CALLBACK_URL,
+				// return_url: 'https://next-pizza.vercel.app/checkout',
+			},
+		},
+		{
+			auth: {
+				username: process.env.YOOKASSA_API_KEY as string,
+				password: '',
+			},
+			headers: {
+				'Idempotency-Key': Math.random().toString(36).substring(7),
+			},
+		}
+	)
+
+	return data
+}
