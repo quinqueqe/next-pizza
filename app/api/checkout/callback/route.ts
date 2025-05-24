b99f6ad74ba1a72235589e0afb6b93a3131622a1
asecer <vrsxdcrown@gmail.com>
2025-05-24 19:16:37 +0300

    (api, chechout, yookassa, prisma) made a status update to an order in the prisma database

---
+++ app/api/checkout/callback/route.ts
@@ -0,0 +1,56 @@
+import { PaymentCallbackData } from '@/@types/yookassa'
+import prisma from '@/prisma/prisma'
+import { OrderSuccessTemplate } from '@/shared/components/shared/email-templates/order-success'
+import { sendEmail } from '@/shared/lib'
+import { CartItemDTO } from '@/shared/services/dto/cart.dto'
+import { OrderStatus } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+	try {
+		const body = (await req.json()) as PaymentCallbackData
+
+		const order = await prisma.order.findFirst({
+			where: {
+				id: Number(body.object.metadata.order_id),
+			},
+			include: {
+				user: true,
+			},
+		})
+
+		if (!order) {
+			return NextResponse.json({ error: 'Order not found' })
+		}
+
+		const isSucceeded = body.object.status === 'succeeded'
+
+		await prisma.order.update({
+			where: {
+				id: order.id,
+			},
+			data: {
+				status: isSucceeded ? OrderStatus.SUCCEEDED : OrderStatus.CANCELLED,
+			},
+		})
+
+		const items = order.items as unknown as CartItemDTO[]
+
+		if (isSucceeded) {
+			await sendEmail(
+				order.email,
+				'Next Pizza | Ваш заказ успешно оформлен ',
+				OrderSuccessTemplate({
+					orderId: order.id,
+					totalAmount: order.totalAmount,
+					fullName: order.fullName,
+					items,
+				})
+			)
+		} else {
+			// письмо о не оплате
+		}
+	} catch (error) {
+		console.log('[GET_ERROR]', error)
+	}
+}
---
+++ shared/components/shared/email-templates/order-success.tsx
@@ -0,0 +1,35 @@
+import { CartItemDTO } from '@/shared/services/dto/cart.dto'
+import React from 'react'
+
+interface Props {
+	orderId: number
+	totalAmount: number
+	fullName: string
+	items: CartItemDTO[]
+}
+
+export const OrderSuccessTemplate = ({
+	orderId,
+	totalAmount,
+	fullName,
+	items,
+}: Props) => (
+	<div>
+		<h3>Здравствуйте, {fullName}!</h3>
+		<p>Благодарим вас за покупку!</p>
+		<p>
+			Ваш заказ #{orderId} на сумму {totalAmount} оплачен
+		</p>
+		<p>Список товаров:</p>
+
+		<ul>
+			{items.map(item => (
+				<li key={item.id}>
+					{item.productItem.product.name} | {item.productItem.price} ₽ x{' '}
+					{item.quantity} шт. ={' '}
+					{(item.productItem.price as number) * item.quantity}
+				</li>
+			))}
+		</ul>
+	</div>
+)
