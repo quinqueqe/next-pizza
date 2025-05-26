83dbf8e2dc7ed7a9f7387523e6fc1492c2231f74
asecer <vrsxdcrown@gmail.com>
2025-05-26 15:21:42 +0300

    (auth, github) made a proper authentication on github with saving data to prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,24 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { compare } from 'bcrypt'
+import { compare, hashSync } from 'bcrypt'
+import { UserRole } from '@prisma/client'
+import { pid } from 'process'
 
 export const authOptions = {
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID || '',
 			clientSecret: process.env.GITHUB_SECRET || '',
+			profile(profile) {
+				return {
+					id: profile.id,
+					name: profile.name || profile.login, // получаем имя или логин именно пользователя с гитхаба
+					email: profile.email,
+					image: profile.avatar_url,
+					role: 'USER' as UserRole,
+				}
+			},
 		}),
 		CredentialsProvider({
 			name: 'Credentials',
@@ -54,7 +65,60 @@ export const authOptions = {
 		strategy: 'jwt',
 	},
 	callbacks: {
+		async signIn({ user, account }) {
+			// GITHUB
+			try {
+				if (account?.provider === 'credentials') {
+					return true
+				}
+
+				if (!user.email) return false
+
+				const findUser = await prisma?.user.findFirst({
+					where: {
+						OR: [
+							{
+								provider: account?.provider,
+								providerId: account?.providerAccountId,
+							},
+							{
+								email: user.email,
+							},
+						],
+					},
+				})
+
+				if (findUser) {
+					await prisma?.user.update({
+						where: {
+							id: findUser.id,
+						},
+						data: {
+							provider: account?.provider,
+							providerId: account?.providerAccountId,
+						},
+					})
+
+					return true
+				}
+
+				await prisma?.user.create({
+					data: {
+						email: user.email,
+						fullName: user.name || `User #${user.id}`,
+						password: hashSync(user.id.toString(), 10), // не желательно делать в виде айди
+						verified: new Date(),
+						provider: account?.provider,
+						providerId: account?.providerAccountId,
+					},
+				})
+			} catch (err) {
+				console.log('[SIGN_IN_ERROR]', err)
+				return false
+			}
+		},
 		async jwt({ token }) {
+			// credentials
 			const findUser = await prisma?.user.findFirst({
 				where: {
 					email: token.email,
