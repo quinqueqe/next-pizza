e71a447a08cbdbbc57492499e8b2555d79ea4d9a
asecer <vrsxdcrown@gmail.com>
2025-05-29 21:27:53 +0300

    (fix, design) made the cart button follow the user in the top-bar

--- shared/components/shared/top-bar.tsx
+++ shared/components/shared/top-bar.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import React from 'react'
 import { Category } from '@prisma/client'
@@ -10,17 +10,28 @@ interface Props {
 }
 
 export const TopBar = ({ data }: Props) => {
-	const scroll = window.scrollY
+	const [scrollPos, setScrollPos] = React.useState(0)
+
+	React.useEffect(() => {
+		const handleScroll = () => {
+			const currentScroll = window.scrollY
+			setScrollPos(currentScroll)
+		}
+		window.addEventListener('scroll', handleScroll)
+	}, [])
 	return (
 		<div
 			className={cn(
-				'sticky top-0 z-20 py-5 bg-white',
-				scroll > 170 && 'shadow-[0_4px_30px_rgba(6,5,50,0.1)]'
+				'sticky top-0 z-20 bg-white',
+				scrollPos > 170 && 'shadow-[0_4px_30px_rgba(6,5,50,0.1)]',
+				// 'py-5'
 			)}
 		>
-			<Container className='flex justify-between items-center py-3'>
+			<Container
+				className={cn('flex justify-between items-center py-3', 'h-[68px]')}
+			>
 				<Categories categories={data} />
-				{/* <CartButton /> */}
+				{scrollPos > 170 && <CartButton />}
 			</Container>
 		</div>
 	)
