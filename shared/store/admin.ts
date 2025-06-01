1e551cbd9d133016bd1976b6ac0557f2421730d4
asecer <vrsxdcrown@gmail.com>
2025-06-01 13:56:56 +0300

    (fix, zustand) removed all detools from the zustand repositories

--- shared/store/admin.ts
+++ shared/store/admin.ts
@@ -1,14 +1,11 @@
 import { create } from 'zustand'
-import { devtools } from 'zustand/middleware'
 
 type AdminType = {
 	role: string
 	setRole: (value: string) => void
 }
 
-export const useAdmin = create<AdminType>()(
-	devtools(set => ({
-		role: 'USER',
-		setRole: value => set({ role: value }),
-	}))
-)
+export const useAdmin = create<AdminType>()(set => ({
+	role: 'USER',
+	setRole: value => set({ role: value }),
+}))
