219a52eadddf3e391e49ec6ebb4cd289342abb09
asecer <vrsxdcrown@gmail.com>
2025-06-02 21:37:26 +0300

    (fix, header, layout) changed the header-provider due to an error in the deployment

--- shared/components/shared/header-provider.tsx
+++ shared/components/shared/header-provider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Header } from './header'
 
 type Props = {
@@ -13,6 +13,8 @@ export const HeaderProvider = ({
 	hasProfile = false,
 }: Props) => {
 	return (
-		<Header hasSearch={hasSearch} hasCart={hasCart} hasProfile={hasProfile} />
+		<Suspense>
+			<Header hasSearch={hasSearch} hasCart={hasCart} hasProfile={hasProfile} />
+		</Suspense>
 	)
 }
