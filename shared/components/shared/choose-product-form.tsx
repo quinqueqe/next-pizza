b3cb41abe4439b7af7314996635af9d1351a48bf
asecer <vrsxdcrown@gmail.com>
2025-06-03 19:08:03 +0300

    (fix, adaptive) made an adaptive for the modal window of other products

--- shared/components/shared/choose-product-form.tsx
+++ shared/components/shared/choose-product-form.tsx
@@ -37,10 +37,10 @@ export const ChooseProductForm = ({
 
 	return (
 		<div>
-			<div className='flex justify-between items-center'>
+			<div className='flex justify-between items-center max-[1100px]:flex-col max-[1100px]:px-4 max-[1100px]:pt-5 max-[1100px]:overflow-auto max-[1100px]:h-[100vh] max-[1100px]:justify-center max-[1100px]:scroll-auto max-[1100px]:relative max-[1100px]:gap-[20px]'>
 				<div>
 					<PizzaImage
-						className='w-[550px] h-[500px] flex justify-center items-center'
+						className='w-[550px] h-[500px] max-[1100px]:w-[300px] max-[1100px]:h-[300px] max-[1100px]:w-full max-[1100px]:h-full flex justify-center items-center'
 						imageUrl={imageUrl}
 						size={30}
 					/>
@@ -48,6 +48,7 @@ export const ChooseProductForm = ({
 				<div
 					className={cn(
 						'bg-[#F4F1EE] h-[610px] p-10 flex flex-col justify-between',
+						'max-[1100px]:p-0 bg-white',
 						rightBlockClassName
 					)}
 				>
@@ -59,7 +60,7 @@ export const ChooseProductForm = ({
 						<p className='pb-6'>{desc}</p>
 					</div>
 					<div>
-						<div>
+						<div className='max-[1100px]:pb-5'>
 							<Button
 								status={status}
 								onClick={() => onClickAdd()}
