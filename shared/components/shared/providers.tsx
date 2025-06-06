da36ed768f1ce5df639616d9e87fc04fce9be78b
asecer <vrsxdcrown@gmail.com>
2025-06-06 22:30:31 +0300

    (vercel) hooked up vercel analytics

--- shared/components/shared/providers.tsx
+++ shared/components/shared/providers.tsx
@@ -1,9 +1,10 @@
-'use client';
+'use client'
 
 import React from 'react'
 import { Toaster } from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
 import NextTopLoader from 'nextjs-toploader'
+import { Analytics } from '@vercel/analytics/next'
 
 type Props = {
 	children: React.ReactNode
@@ -15,6 +16,7 @@ export const Providers = ({ children }: Props) => {
 			<SessionProvider>{children}</SessionProvider>
 			<Toaster />
 			<NextTopLoader />
+			<Analytics />
 		</>
 	)
 }
