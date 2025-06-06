be08423be854c4306486a1fd15b2afc0fa76b9dc
asecer <vrsxdcrown@gmail.com>
2025-06-06 22:22:18 +0300

    (fix, adaptive) fixed errors related to modal window adaptation for products

--- shared/components/shared/choose-pizza-form.tsx
+++ shared/components/shared/choose-pizza-form.tsx
@@ -66,7 +66,8 @@ export const ChoosePizzaForm = ({
 	}
 	return (
 		<div>
-			<div className='flex justify-between relative items-center max-[1100px]:flex-col max-[1100px]:px-4 max-[1100px]:pt-17 max-[1100px]:h-[100vh] max-[1100px]:justify-center max-[1100px]:scroll-auto max-[1100px]:relative max-[1100px]:gap-[20px] max-[1100px]:relative'>
+			<div className='flex justify-between relative items-center max-[1100px]:flex-col max-[1100px]:px-4 max-[1100px]:pt-18 max-[1100px]:h-[100vh] max-[1100px]:justify-center max-[1100px]:relative max-[1100px]:gap-[20px] max-[1100px]:relative max-[1100px]:overflow-auto max-[1100px]:scroll-auto'>
+				
 				<div className=' max-[1100px]:bg-[rgb(255,255,255)]'>
 					<PizzaImage
 						className='w-[550px] h-[500px] flex justify-center items-center max-[1100px]:w-full max-[1100px]:h-full'
@@ -124,7 +125,7 @@ export const ChoosePizzaForm = ({
 								/>
 							))}
 						</ul>
-						<div className='max-[1100px]:sticky max-[1100px]:bottom-0 max-[1100px]:left-0 max-[1100px]:py-4 max-[1100px]:px-4 max-[1100px]:bg-white max-[1100px]:z-8 max-[1100px]:-mx-4 max-[470px]:shadow-[0_4px_30px_rgba(6,5,50,0.1)]'>
+						<div className='max-[1100px]:bottom-0 max-[1100px]:left-0 max-[1100px]:py-4 max-[1100px]:px-4 max-[1100px]:bg-white max-[1100px]:z-8 max-[1100px]:-mx-4 max-[470px]:shadow-[0_4px_30px_rgba(6,5,50,0.1)] max-[1100px]:mb-5'>
 							<Button
 								status={status}
 								onClick={() => handleClickAdd()}
